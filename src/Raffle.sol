b27646f034bbe3f1b006e35775af320018b69f95
Ismail Dawoodjee <ismail.dawoodjee27@gmail.com>
2025-01-06 21:51:31 +0700

    Add more Raffle contract functionality and renames

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,25 +9,57 @@ pragma solidity 0.8.28;
  */
 contract Raffle {
     /* STATE VARIABLES */
-    // entrance fee for entering the raffle
-    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_raffleEntranceFee; // Entrance fee for entering the raffle
+    address payable[] private s_listOfPlayers;
+    uint256 private immutable i_lotteryDurationSeconds; // How long each round of lottery lasts before picking a winner
+    uint256 private s_lastRecordedTimeStamp; // The last timestamp that was recorded
+
+    /* EVENTS */
+    event NewPlayerHasEnteredRaffle(address indexed _newPlayer);
 
     /* ERRORS */
-    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__NotEnoughEthToEnterRaffle();
 
     /* CONSTRUCTOR */
-    constructor(uint256 _entranceFee) {
-        i_entranceFee = _entranceFee;
+    constructor(uint256 _raffleEntranceFee, uint256 _lotteryDurationSeconds) {
+        i_raffleEntranceFee = _raffleEntranceFee;
+        i_lotteryDurationSeconds = _lotteryDurationSeconds;
+        s_lastRecordedTimeStamp = block.timestamp; // Initialized to when the contract is first deployed (first block)
     }
 
-    function enterRaffle() public payable {
-        if (msg.value <= i_entranceFee) revert Raffle__SendMoreToEnterRaffle();
+    function enterRaffle() external payable {
+        if (msg.value < i_raffleEntranceFee) revert Raffle__NotEnoughEthToEnterRaffle();
+
+        s_listOfPlayers.push(payable(msg.sender)); // Player is going to be the address that called this function
+        emit NewPlayerHasEnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    /**
+     * 1. This function needs to get a random number from VRF
+     * 2. Then it needs to use the random number to pick out a winner
+     * 3. Finally, it needs to be called automatically, at periodic intervals
+     *
+     * Getting a random number is a two-transaction process:
+     * 1. First, we send a transaction to request a random number from the subscription
+     * 2. In the next transaction, we get that random number from the oracle
+     */
+    function pickRaffleWinner() external {
+        // We want to check to see if enough time has passed before picking the winner
+        if (block.timestamp - s_lastRecordedTimeStamp > i_lotteryDurationSeconds) {
+            revert();
+        }
+    }
 
     /* GETTER FUNCTIONS */
-    function getEntranceFee() external view returns (uint256) {
-        return i_entranceFee;
+    function getRaffleEntranceFee() external view returns (uint256) {
+        return i_raffleEntranceFee;
+    }
+
+    function getPlayerAddress(uint256 _playerIndex) external view returns (address) {
+        return s_listOfPlayers[_playerIndex];
+    }
+
+    function getLotteryDuration() external view returns (uint256) {
+        return i_lotteryDurationSeconds;
     }
 }
